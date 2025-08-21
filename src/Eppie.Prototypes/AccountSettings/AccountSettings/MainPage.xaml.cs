63c77394eeaa57c0af64e19e83ca004d6df98314
alexander <kauhan+github@eppie.io>
2025-08-21 16:33:13 +0300

    update: Advanced settings switch

--- src/Eppie.Prototypes/AccountSettings/AccountSettings/MainPage.xaml.cs
+++ src/Eppie.Prototypes/AccountSettings/AccountSettings/MainPage.xaml.cs
@@ -1,19 +1,46 @@
+using Microsoft.UI.Xaml.Controls.Primitives;
+
 namespace AccountSettings;
 
 public sealed partial class MainPage : Page
 {
+    public bool IsAdvancedMode
+    {
+        get { return (bool)GetValue(IsAdvancedModeProperty); }
+        set { SetValue(IsAdvancedModeProperty, value); }
+    }
+
+    public static readonly DependencyProperty IsAdvancedModeProperty =
+        DependencyProperty.Register(nameof(IsAdvancedMode), typeof(bool), typeof(MainPage), new PropertyMetadata(false, OnAdvancedModeChanged));
+
+    private static void OnAdvancedModeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is MainPage mainPage && e.NewValue is bool isAdvancedMode)
+        {
+            if (isAdvancedMode)
+            {
+                mainPage.RotateChevronTo180();
+            }
+            else
+            {
+                mainPage.RotateChevronTo0();
+            }
+        }
+    }
+
     public MainPage()
     {
         this.InitializeComponent();
 
-        SettingsModeSelector.SelectedIndex = 0;
+        // The first version of the 'Advanced settings' switch
+        // SettingsModeSelector.SelectedIndex = 0;
     }
 
     private void OnSettingModeChanged(object sender, SelectionChangedEventArgs e)
     {
         if (sender is ComboBox combobox)
         {
-            CustomSection.Visibility = combobox.SelectedIndex == 1 ? Visibility.Visible : Visibility.Collapsed;
+            IsAdvancedMode = combobox.SelectedIndex == 1;
         }
     }
 
@@ -30,5 +57,20 @@ public sealed partial class MainPage : Page
             ApplyText.Opacity = 1;
         }
         catch { }
+    }
+
+    public void RotateChevronTo180()
+    {
+        RotateChevronTo180Degrees.Begin();
+    }
+
+    public void RotateChevronTo0()
+    {
+        RotateChevronTo0Degrees.Begin();
+    }
+
+    private void OnSecondPasswordInfoClick(object sender, RoutedEventArgs e)
+    {
+        FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
     }
 }
